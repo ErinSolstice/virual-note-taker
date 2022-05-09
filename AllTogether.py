003f943061419969cc44e3ec45160ed6ae579bab
Marbles552 <nospfo@gmail.com>
2022-05-09 16:41:14 -0500

    Autocorrect Upload

--- AllTogether.py
+++ AllTogether.py
@@ -1,13 +1,13 @@
 # from matplotlib import pyplot as plt
+import cv2
 import easyocr
 # import argparse
 import numpy as np
 from fpdf import FPDF
 from autocorrect import Speller
-import cv2
 
 
-img_name = "sampleImages/dataset"
+img_name = "sampleImages/slide"
 img_type = ".png"
 img = cv2.imread(img_name+img_type)
 img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -19,15 +19,14 @@ img_copy = img.copy()
 # cv2.imshow("img", img)
 # cv2.imshow("img_grey", img_grey)
 # cv2.imshow("img_blur", img_blur)
-cv2.imshow("img_canny", img_canny)
-cv2.imshow("img_dil", img_dil)
+# cv2.imshow("img_canny", img_canny)
+# cv2.imshow("img_dil", img_dil)
 cv2.drawContours(image=img_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=1, lineType=cv2.LINE_AA)
 cv2.imshow("img_contour", img_copy)
 cv2.waitKey(0)
 
 reader = easyocr.Reader(['en'])  # this needs to run only once to load the model into memory
 # Patrick Update with New Model
-
 
 # Use OCR model to extract text
 results = reader.readtext(img)
@@ -108,9 +107,10 @@ pdf.add_page()
 # Loop through results and save text to PDF
 for x in range(i):
     pdf.set_xy(results[x][0][0][0]*scale, results[x][0][0][1]*scale)
-    pdf.set_font("Arial", size=(results[x][0][2][1] - results[x][0][0][1]-3)*scale)
+    pdf.set_font("Arial", size=(results[x][0][2][1] - results[x][0][0][1]-3)*scale*0.8)
     pdf.cell((results[x][0][1][0] - results[x][0][0][0])*scale, (results[x][0][2][1] - results[x][0][0][1])*scale,
-             txt=spell(results[x][1]))
+             txt=results[x][1])
+# txt=spell(results[x][1]))
 
 # Save output PDF
 pdf.output(img_name + '.pdf')
